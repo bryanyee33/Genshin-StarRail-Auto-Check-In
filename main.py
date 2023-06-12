60f605eaa9b240fa96b3a5b6682b0b293165d3ca
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2023-06-12 10:23:18 +0800

    Allow for multiple accounts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from hoyo_checkin import hoyo_checkin
+from hoyo_checkin import game_perform_checkin
 
 logging.basicConfig(
     level=logging.INFO,
@@ -8,19 +8,17 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%S"
 )
 
+DEFAULT_LANGUAGE = "en-us"
 OS_COOKIE = os.environ.get('OS_COOKIE')
 
-#genshin
-hoyo_checkin(
-    "https://hk4e-api-os.mihoyo.com/event/sol",
-    "e202102251931481",
-    OS_COOKIE,
-)
+games = ["genshin", "starrail"] # honkai and themis also available
 
-#star rail
-hoyo_checkin(
-    "https://sg-public-api.hoyolab.com/event/luna/os",
-    "e202303301540311",
-    OS_COOKIE,
-)
+for i, cookie in enumerate(OS_COOKIE.split(","), start = 1):
+    for game in games:
+        game_perform_checkin(
+            i,
+            game,
+            cookie,
+            DEFAULT_LANGUAGE
+        )
 
