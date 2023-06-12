66a9b5ae1db0ef58e941309a4dc8f72427bf91ff
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2023-06-12 10:24:43 +0800

    Update http_helper.py

--- http_helper.py
+++ http_helper.py
@@ -1,16 +1,14 @@
-import os
 import json
 import logging
 
 import requests
 from requests import HTTPError, Response
 
-default_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-                  
-def get_user_agent():
-    if "USER_AGENT" in os.environ:
-        return os.environ["USER_AGENT"]
-    return default_user_agent
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/74.0.3729.169 Safari/537.36"
+)
 
 
 def http_get(url: str, max_retries: int = 2, **kwargs) -> Response:
@@ -34,24 +32,27 @@ def http_post_json(url: str, max_retries: int = 2, **kwargs):
 
 
 def http_request(
-    method: str,
-    url: str,
-    max_retries: int = 2,
-    **kwargs
+        method: str,
+        url: str,
+        max_retries: int = 2,
+        **kwargs,
 ) -> Response:
     for i in range(max_retries + 1):
         try:
-            logging.debug(f"{method.upper()} {url}, REQ: {i+1}/{max_retries}")
+            logging.debug(f"{method.upper()} {url}, REQ: {i + 1}/{max_retries}")
             session = requests.Session()
-            session.headers["USER_AGENT"] = get_user_agent()
+            session.headers["User-Agent"] = USER_AGENT
             resp = session.request(method, url, **kwargs)
-            logging.debug(f"Response: {resp.status_code}\n\n{resp.text}\n")
+
+            text = resp.text
+            if resp.headers.get("Content-Type", "") == "application/json":
+                text = json.loads(text)
+            logging.debug(f"Response: {resp.status_code}\n\n{text}\n")
         except HTTPError as e:
-            logging.error(f"HTTP error: {e}, REQ: {i+1}/{max_retries}")
+            logging.error(f"HTTP error: {e}, REQ: {i + 1}/{max_retries}")
         except KeyError as e:
-            logging.error(f"Wrong response: {e}, REQ: {i+1}/{max_retries}")
-        except Exception as e:
-            logging.error(f"Unknown error: {e}, REQ: {i+1}/{max_retries}")
+            logging.error(f"Wrong response: {e}, REQ: {i + 1}/{max_retries}")
         else:
             return resp
-    raise Exception(f"All {max_retries} HTTP requests have failed")
+    msg = f"All {max_retries} HTTP requests have failed"
+    raise Exception(msg)
