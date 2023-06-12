44494073472ca0adcff76ddc99d8a8d36eb0e33b
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2023-06-12 10:25:12 +0800

    Update hoyo_checkin.py

--- hoyo_checkin.py
+++ hoyo_checkin.py
@@ -1,26 +1,69 @@
 import json
+import logging
 import random
 import time
-import logging
 
 from http_helper import http_get_json, http_post_json
 
-
 RET_CODE_ALREADY_SIGNED_IN = -5003
 
+GAMES = {
+    "genshin": {
+        "name": "Genshin Impact",
+        "event_base_url": "https://hk4e-api-os.mihoyo.com/event/sol",
+        "act_id": "e202102251931481",
+        "login_url": "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html"
+                     "?act_id=e202102251931481",
+    },
+    "starrail": {
+        "name": "Honkai: Star Rail",
+        "event_base_url": "https://sg-public-api.hoyolab.com/event/luna/os",
+        "act_id": "e202303301540311",
+        "login_url": "https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html"
+                     "?act_id=e202303301540311",
+    },
+    "honkai": {
+        "name": "Honkai Impact 3rd",
+        "event_base_url": "https://sg-public-api.hoyolab.com/event/mani",
+        "act_id": "e202110291205111",
+        "login_url": "https://act.hoyolab.com/bbs/event/signin-bh3/index.html"
+                     "?act_id=e202110291205111",
+    },
+    "themis": {
+        "name": "Tears of Themis",
+        "event_base_url": "https://sg-public-api.hoyolab.com/event/luna/os",
+        "act_id": "e202202281857121",
+        "login_url": "https://act.hoyolab.com/bbs/event/signin/nxx/index.html"
+                     "?act_id=e202202281857121",
+    },
+}
+_CAPTCHA_MESSAGE = """Captcha is required, please sign into the website: %s"""
 
-def hoyo_checkin(
-    event_base_url: str,
-    act_id: str,
-    cookie_str: str
+
+def game_perform_checkin(
+        account_ident: str,
+        game: str,
+        cookie_str: str,
+        language: str,
+        skip_checkin: bool = False,
 ):
-    lang = "en-us"
+    if game not in GAMES:
+        msg = f"unknown game identifier found: {game}"
+        raise Exception(msg)
+
+    game_name = GAMES[game]["name"]
+    event_base_url = GAMES[game]["event_base_url"]
+    act_id = GAMES[game]["act_id"]
+    login_url = GAMES[game]["login_url"]
+
     referer_url = "https://act.hoyolab.com/"
-    reward_url = f"{event_base_url}/home?lang={lang}" \
-                 f"&act_id={act_id}"
-    info_url = f"{event_base_url}/info?lang={lang}" \
-               f"&act_id={act_id}"
-    sign_url = f"{event_base_url}/sign?lang={lang}"
+    reward_url = (
+        f"{event_base_url}/home?lang={language}&act_id={act_id}"
+    )
+    info_url = (
+        f"{event_base_url}/info?lang={language}&act_id={act_id}"
+    )
+    sign_url = f"{event_base_url}/sign?lang={language}"
 
     headers = {
         "Referer": referer_url,
@@ -30,10 +73,17 @@ def hoyo_checkin(
 
     info_list = http_get_json(info_url, headers=headers)
 
+    if not info_list.get("data"):
+        message = info_list.get("message", "None")
+        logging.error(f"Could not retrieve data from API endpoint: {message}")
+        return
+
     today = info_list.get("data", {}).get("today")
     total_sign_in_day = info_list.get("data", {}).get("total_sign_day")
     already_signed_in = info_list.get("data", {}).get("is_sign")
     first_bind = info_list.get("data", {}).get("first_bind")
+
+    logging.info(f"Attempting checking for game {game_name} ({account_ident})")
 
     if already_signed_in:
         logging.info("Already checked in today")
@@ -58,7 +108,16 @@ def hoyo_checkin(
         "act_id": act_id,
     }, ensure_ascii=False)
 
-    response = http_post_json(sign_url, headers=headers, data=request_data)
+    if not skip_checkin:
+        response = http_post_json(sign_url, headers=headers, data=request_data)
+    else:
+        response = {
+            "retcode": 0,
+            "message": "Test Run, skipped actual checkin request",
+        }
+
+    # as we logged in for a day, the number of total sign ins has to increase
+    total_sign_in_day += 1
 
     code = response.get("retcode", 99999)
 
@@ -67,13 +126,32 @@ def hoyo_checkin(
     if code == RET_CODE_ALREADY_SIGNED_IN:
         logging.info("Already signed in for today...")
         return
-    elif code != 0:
-        logging.error(response['message'])
+
+    if is_captcha_required(response):
+        msg = _CAPTCHA_MESSAGE % login_url
+        logging.error(msg)
+        return
+
+    if code != 0:
+        logging.error(response["message"])
         return
 
     reward = awards[total_sign_in_day - 1]
 
     logging.info("Check-in complete!")
-    logging.info(f"\tTotal Sign-in Days: {total_sign_in_day + 1}")
+    logging.info(f"\tTotal Sign-in Days: {total_sign_in_day}")
     logging.info(f"\tReward: {reward['cnt']}x {reward['name']}")
     logging.info(f"\tMessage: {response['message']}")
+
+
+def is_captcha_required(response: dict) -> bool:
+    if "gt_result" not in response:
+        return False
+
+    gt = response["gt_result"]["gt"]
+    challenge = response["gt_result"]["challenge"]
+
+    if not gt and not challenge:
+        return False
+
+    return True
